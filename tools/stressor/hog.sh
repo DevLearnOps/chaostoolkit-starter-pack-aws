2ed5b0f9b971efc0f7a776fd85dbc7cf853eaf7a
Manuel Castellin <manuel@castellinconsulting.com>
2023-06-25 08:07:36 +0200

    add todo

--- tools/stressor/hog.sh
+++ tools/stressor/hog.sh
@@ -2,5 +2,9 @@
 echo "Executing container hogs: stress-ng ${HOG_CONFIG}"
 SLEEP=${HOG_DELAY:-60}
 
+# TODO: ability to randomly wait a certain amount of time before injecting the stressor
+# this is useful to randomnly render services unresponsive and check the load balancer 
+# can detect and replace them.
+
 eval "sleep ${SLEEP}"
 exec $(eval "stress-ng ${HOG_CONFIG}")
