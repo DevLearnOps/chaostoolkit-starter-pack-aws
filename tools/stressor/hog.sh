3401b056fab8fa1e23a74d05c87c6ebd19fe7a1a
Manuel Castellin <manuel@castellinconsulting.com>
2023-06-25 17:01:30 +0200

    add github actions

--- tools/stressor/hog.sh
+++ tools/stressor/hog.sh
@@ -3,7 +3,7 @@ echo "Executing container hogs: stress-ng ${HOG_CONFIG}"
 SLEEP=${HOG_DELAY:-60}
 
 # TODO: ability to randomly wait a certain amount of time before injecting the stressor
-# this is useful to randomnly render services unresponsive and check the load balancer 
+# this is useful to randomly render services unresponsive and check the load balancer 
 # can detect and replace them.
 
 eval "sleep ${SLEEP}"
