59f4fea2850f1b5fe1bcb88940f9156fdb931ad8
Manuel Castellin <manuel@castellinconsulting.com>
2023-07-07 16:12:00 +0200

    align outputs with new names

--- infrastructure/full/outputs.tf
+++ infrastructure/full/outputs.tf
@@ -6,13 +6,13 @@ output "internal_url" {
 }
 
 resource "aws_ssm_parameter" "application_web_url" {
-  name  = "/app/${var.application_name}/${var.environment}/web_url"
+  name  = "/${var.environment}/app/${var.application_name}/web_url"
   value = "http://${module.public_alb.lb_dns_name}"
   type  = "String"
 }
 
 resource "aws_ssm_parameter" "internal_alb_dns_name" {
-  name  = "/app/${var.application_name}/${var.environment}/internal_alb_dns_name"
+  name  = "/${var.environment}/app/${var.application_name}/internal_alb_dns_name"
   value = module.internal_alb.lb_dns_name
   type  = "String"
 }
