ff4480c445421d14b1b5deae087c010a6ffd843b
Manuel Castellin <manuel@castellinconsulting.com>
2023-08-11 09:09:46 +0200

    chore: refactor dashbaord to define rows position as locals

--- infrastructure/comments-app/services/dashboard.tf
+++ infrastructure/comments-app/services/dashboard.tf
@@ -1,3 +1,15 @@
+locals {
+  cw_row_height_medium = 6
+  cw_row_height_small  = 3
+
+  cw_row_alarms       = 0
+  cw_row_logs         = 6
+  cw_row_req_count    = 12
+  cw_row_req_time     = 18
+  cw_row_svc_insights = 24
+  cw_row_asg          = 48
+}
+
 resource "aws_cloudwatch_dashboard" "main_dashboard" {
   dashboard_name = "${local.name}-app-dashboard"
 
@@ -6,9 +18,9 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
       {
         "type" : "metric",
         "x" : 0,
-        "y" : 0,
+        "y" : local.cw_row_alarms,
         "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             [{ "expression" : "SELECT SUM(RequestCountPerTarget) FROM SCHEMA(\"AWS/ApplicationELB\", TargetGroup) GROUP BY TargetGroup", "label" : "RequestCountPerTarget", "id" : "q1", "period" : 1 }]
@@ -24,9 +36,9 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
       {
         "type" : "alarm",
         "x" : 8,
-        "y" : 0,
+        "y" : local.cw_row_alarms,
         "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "title" : "Alarm Status",
           "alarms" : [
@@ -40,51 +52,35 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
         }
       },
       {
-        "type" : "metric",
+        "type" : "log",
         "x" : 0,
-        "y" : 18,
-        "width" : 8,
-        "height" : 6,
+        "y" : local.cw_row_logs,
+        "width" : 24,
+        "height" : local.cw_row_height_medium,
         "properties" : {
-          "metrics" : [
-            ["AWS/ECS", "CPUUtilization", "ServiceName", "${module.web_service.name}", "ClusterName", "${module.app_cluster.name}", { "id" : "m1", "stat" : "Average", "region" : "${local.region}" }],
-            ["...", { "id" : "m2", "region" : "${local.region}" }],
-            ["...", { "id" : "m3", "stat" : "Maximum", "region" : "${local.region}" }]
-          ],
-          "view" : "timeSeries",
-          "stacked" : false,
+          "query" : "SOURCE '/aws/batch/job' | fields @message\n| sort @timestamp desc\n| limit 50",
           "region" : "${local.region}",
-          "stat" : "Minimum",
-          "period" : 10,
-          "title" : "Comments Web - CPU Utilization"
+          "stacked" : false,
+          "view" : "table",
+          "title" : "Chaos Logs"
+        }
+      },
+      {
+        "type" : "text",
+        "x" : 0,
+        "y" : local.cw_row_req_count,
+        "width" : 24,
+        "height" : local.cw_row_height_small,
+        "properties" : {
+          "markdown" : "# Request Statistics\n\nStats regarding the requests count by service"
         }
       },
       {
         "type" : "metric",
         "x" : 8,
-        "y" : 18,
+        "y" : local.cw_row_req_count,
         "width" : 8,
-        "height" : 6,
-        "properties" : {
-          "metrics" : [
-            ["AWS/ECS", "MemoryUtilization", "ServiceName", "${module.web_service.name}", "ClusterName", "${module.app_cluster.name}", { "id" : "m1", "stat" : "Average", "region" : "${local.region}" }],
-            ["...", { "id" : "m2", "region" : "${local.region}" }],
-            ["...", { "id" : "m3", "stat" : "Maximum", "region" : "${local.region}" }]
-          ],
-          "view" : "timeSeries",
-          "stacked" : false,
-          "region" : "${local.region}",
-          "stat" : "Minimum",
-          "period" : 10,
-          "title" : "Comments Web - Memory Utilization"
-        }
-      },
-      {
-        "type" : "metric",
-        "x" : 8,
-        "y" : 9,
-        "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             ["AWS/ApplicationELB", "HTTPCode_Target_2XX_Count", "TargetGroup", "${module.internal_alb.target_group_arn_suffixes[0]}", "LoadBalancer", "${module.internal_alb.lb_arn_suffix}", { "region" : "${local.region}" }],
@@ -102,9 +98,9 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
       {
         "type" : "metric",
         "x" : 16,
-        "y" : 9,
+        "y" : local.cw_row_req_count,
         "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             ["AWS/ApplicationELB", "HTTPCode_Target_2XX_Count", "TargetGroup", "${module.internal_alb.target_group_arn_suffixes[1]}", "LoadBalancer", "${module.internal_alb.lb_arn_suffix}"],
@@ -122,9 +118,9 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
       {
         "type" : "metric",
         "x" : 0,
-        "y" : 9,
+        "y" : local.cw_row_req_count,
         "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             ["AWS/ApplicationELB", "HTTPCode_Target_2XX_Count", "TargetGroup", "${module.public_alb.target_group_arn_suffixes[0]}", "LoadBalancer", "${module.public_alb.lb_arn_suffix}", { "region" : "${local.region}" }],
@@ -142,9 +138,9 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
       {
         "type" : "metric",
         "x" : 8,
-        "y" : 9,
+        "y" : local.cw_row_req_time,
         "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             ["AWS/ApplicationELB", "TargetResponseTime", "TargetGroup", "${module.internal_alb.target_group_arn_suffixes[0]}", "LoadBalancer", "${module.internal_alb.lb_arn_suffix}", { "id" : "m1" }],
@@ -162,9 +158,9 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
       {
         "type" : "metric",
         "x" : 16,
-        "y" : 9,
+        "y" : local.cw_row_req_time,
         "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             ["AWS/ApplicationELB", "TargetResponseTime", "TargetGroup", "${module.internal_alb.target_group_arn_suffixes[1]}", "LoadBalancer", "${module.internal_alb.lb_arn_suffix}", { "id" : "m1" }],
@@ -182,9 +178,9 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
       {
         "type" : "metric",
         "x" : 0,
-        "y" : 9,
+        "y" : local.cw_row_req_time,
         "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             ["AWS/ApplicationELB", "TargetResponseTime", "TargetGroup", "${module.public_alb.target_group_arn_suffixes[0]}", "LoadBalancer", "${module.public_alb.lb_arn_suffix}", { "id" : "m1" }],
@@ -200,11 +196,61 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
         }
       },
       {
+        "type" : "text",
+        "x" : 0,
+        "y" : local.cw_row_svc_insights,
+        "width" : 24,
+        "height" : local.cw_row_height_small,
+        "properties" : {
+          "markdown" : "# ECS Services Insights\n\nCPU + Memory Utilization and Desired vs Running container count"
+        }
+      },
+      {
+        "type" : "metric",
+        "x" : 0,
+        "y" : local.cw_row_svc_insights,
+        "width" : 8,
+        "height" : local.cw_row_height_medium,
+        "properties" : {
+          "metrics" : [
+            ["AWS/ECS", "CPUUtilization", "ServiceName", "${module.web_service.name}", "ClusterName", "${module.app_cluster.name}", { "id" : "m1", "stat" : "Average", "region" : "${local.region}" }],
+            ["...", { "id" : "m2", "region" : "${local.region}" }],
+            ["...", { "id" : "m3", "stat" : "Maximum", "region" : "${local.region}" }]
+          ],
+          "view" : "timeSeries",
+          "stacked" : false,
+          "region" : "${local.region}",
+          "stat" : "Minimum",
+          "period" : 10,
+          "title" : "Comments Web - CPU Utilization"
+        }
+      },
+      {
+        "type" : "metric",
+        "x" : 8,
+        "y" : local.cw_row_svc_insights,
+        "width" : 8,
+        "height" : local.cw_row_height_medium,
+        "properties" : {
+          "metrics" : [
+            ["AWS/ECS", "MemoryUtilization", "ServiceName", "${module.web_service.name}", "ClusterName", "${module.app_cluster.name}", { "id" : "m1", "stat" : "Average", "region" : "${local.region}" }],
+            ["...", { "id" : "m2", "region" : "${local.region}" }],
+            ["...", { "id" : "m3", "stat" : "Maximum", "region" : "${local.region}" }]
+          ],
+          "view" : "timeSeries",
+          "stacked" : false,
+          "region" : "${local.region}",
+          "stat" : "Minimum",
+          "period" : 10,
+          "title" : "Comments Web - Memory Utilization"
+        }
+      },
+      {
         "type" : "metric",
         "x" : 16,
-        "y" : 18,
+        "y" : local.cw_row_svc_insights,
         "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             ["ECS/ContainerInsights", "DesiredTaskCount", "ServiceName", "${module.web_service.name}", "ClusterName", "${module.app_cluster.name}", { "region" : "${local.region}" }],
@@ -219,31 +265,11 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
         }
       },
       {
-        "type" : "text",
-        "x" : 0,
-        "y" : 6,
-        "width" : 24,
-        "height" : 3,
-        "properties" : {
-          "markdown" : "# Request Statistics\n\nStats regarding the requests count by service"
-        }
-      },
-      {
-        "type" : "text",
-        "x" : 0,
-        "y" : 15,
-        "width" : 24,
-        "height" : 3,
-        "properties" : {
-          "markdown" : "# ECS Services Insights\n\nCPU + Memory Utilization and Desired vs Running container count"
-        }
-      },
-      {
         "type" : "metric",
         "x" : 0,
-        "y" : 24,
+        "y" : local.cw_row_svc_insights,
         "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             ["AWS/ECS", "CPUUtilization", "ServiceName", "${module.api_service.name}", "ClusterName", "${module.app_cluster.name}", { "id" : "m1", "stat" : "Average" }],
@@ -261,9 +287,9 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
       {
         "type" : "metric",
         "x" : 8,
-        "y" : 24,
+        "y" : local.cw_row_svc_insights,
         "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             ["AWS/ECS", "MemoryUtilization", "ServiceName", "${module.api_service.name}", "ClusterName", "${module.app_cluster.name}", { "id" : "m1", "stat" : "Average", "region" : "${local.region}" }],
@@ -281,9 +307,9 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
       {
         "type" : "metric",
         "x" : 16,
-        "y" : 24,
+        "y" : local.cw_row_svc_insights,
         "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             ["ECS/ContainerInsights", "DesiredTaskCount", "ServiceName", "${module.api_service.name}", "ClusterName", "${module.app_cluster.name}"],
@@ -300,9 +326,9 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
       {
         "type" : "metric",
         "x" : 0,
-        "y" : 30,
+        "y" : local.cw_row_svc_insights,
         "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             ["AWS/ECS", "CPUUtilization", "ServiceName", "${module.spamcheck_service.name}", "ClusterName", "${module.app_cluster_ec2.cluster_name}", { "id" : "m1", "stat" : "Average", "region" : "${local.region}" }],
@@ -320,9 +346,9 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
       {
         "type" : "metric",
         "x" : 8,
-        "y" : 30,
+        "y" : local.cw_row_svc_insights,
         "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             ["AWS/ECS", "MemoryUtilization", "ServiceName", "${module.spamcheck_service.name}", "ClusterName", "${module.app_cluster_ec2.cluster_name}", { "id" : "m1", "stat" : "Average", "region" : "${local.region}" }],
@@ -340,9 +366,9 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
       {
         "type" : "metric",
         "x" : 16,
-        "y" : 30,
+        "y" : local.cw_row_svc_insights,
         "width" : 8,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             ["ECS/ContainerInsights", "DesiredTaskCount", "ServiceName", "${module.spamcheck_service.name}", "ClusterName", "${module.app_cluster_ec2.cluster_name}"],
@@ -357,11 +383,11 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
         }
       },
       {
-        "height" : 3,
-        "width" : 24,
-        "y" : 36,
-        "x" : 0,
         "type" : "text",
+        "x" : 0,
+        "y" : local.cw_row_asg,
+        "width" : 24,
+        "height" : local.cw_row_height_small,
         "properties" : {
           "markdown" : "# EC2 Container Instances\n\nCPU + Memory Utilization for container instances providing capacity to ECS services"
         }
@@ -369,9 +395,9 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
       {
         "type" : "metric",
         "x" : 0,
-        "y" : 39,
+        "y" : local.cw_row_asg,
         "width" : 6,
-        "height" : 6,
+        "height" : local.cw_row_height_medium,
         "properties" : {
           "metrics" : [
             ["AWS/EC2", "CPUUtilization", "AutoScalingGroupName", "${module.app_cluster_ec2.autoscaling["ondemand"].autoscaling_group_name}", { "stat" : "Average", "region" : "${local.region}" }],
